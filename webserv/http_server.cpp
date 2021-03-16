f9209cbc9a281cd43965dd9fabce6a723ee73dd4
SalimTerryLi <lhf2613@gmail.com>
2021-03-16 16:10:37 +0800

    cleanup for deploy

--- webserv/http_server.cpp
+++ webserv/http_server.cpp
@@ -83,13 +83,14 @@ int start_http_server() {
 	}
 
 	struct lws_context_creation_info info {};
-	int logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
-	        /* for LLL_ verbosity above NOTICE to be built into lws,
+	int logs = LLL_ERR;
+	//LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
+	/* for LLL_ verbosity above NOTICE to be built into lws,
 	 * lws must have been configured and built with
 	 * -DCMAKE_BUILD_TYPE=DEBUG instead of =RELEASE */
-	        /* | LLL_INFO */ /* | LLL_PARSER */ /* | LLL_HEADER */
-	        /* | LLL_EXT */ /* | LLL_CLIENT */  /* | LLL_LATENCY */
-	        /* | LLL_DEBUG */;
+	/* | LLL_INFO */ /* | LLL_PARSER */ /* | LLL_HEADER */
+	/* | LLL_EXT */ /* | LLL_CLIENT */  /* | LLL_LATENCY */
+	/* | LLL_DEBUG */;
 	lws_set_log_level(logs, nullptr);
 	lwsl_user("LWS minimal http server | visit http://localhost:7681\n");
 
